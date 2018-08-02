55c866ba4f49b8f1895ff22d8cd609bc3fbc9486
ogrezem <ogrezem@gmail.com>
2018-08-02 18:49:53 +0300

    123

--- src/main/kotlin/com/ogr/Application.kt
+++ src/main/kotlin/com/ogr/Application.kt
@@ -1,14 +1,9 @@
 package main.kotlin.com.ogr
 
-import java.time.Instant
-import java.util.*
-
 fun main(args: Array<String>) {
     var i = 1
     while(true) {
         println("Do something: ${i++}")
-        print("Введите сообщение: ")
-        println("Ваше сообщение: ${readLine()}")
         Thread.sleep(1000L)
     }
 }
