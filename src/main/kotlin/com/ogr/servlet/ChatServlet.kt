03abaf5a25b33957e0c008b488e5d927d476014a
ogrezem <ogrezem@gmail.com>
2018-08-04 14:37:57 +0300

    123

--- src/main/kotlin/com/ogr/servlet/ChatServlet.kt
+++ src/main/kotlin/com/ogr/servlet/ChatServlet.kt
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletResponse
 class ChatServlet : HttpServlet() {
     var counter = 1
     override fun doPost(req: HttpServletRequest, resp: HttpServletResponse) {
-        resp.writer.print("Otvet dan! Your message: ${req.getParameter("message")}. Counter: ${counter++}")
+        val requestText = req.getParameter("message")
+        println("Текст запроса: $requestText")
+        println("Счётчик $counter")
+        resp.writer.print("Otvet dan! Your message: $requestText. Counter: ${counter++}")
     }
 }
