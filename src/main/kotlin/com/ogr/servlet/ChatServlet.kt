9cc44eb1b71a4d5fc5ab9c9b216ee85ff4b16c00
ogrezem <ogrezem@gmail.com>
2018-08-05 08:52:37 +0300

    123

--- src/main/kotlin/com/ogr/servlet/ChatServlet.kt
+++ src/main/kotlin/com/ogr/servlet/ChatServlet.kt
@@ -31,6 +31,20 @@ class ChatServlet : HttpServlet() {
         var requestText = req.getParameter("message")
         println("Текст GET запроса: $requestText")
         println("Счётчик $counter")
+        val attributeNames = req.attributeNames
+        println("Имена атрибутов: ")
+        if(attributeNames.toList().isEmpty())
+            println("Не было найдено не одного аттрибута")
+        while(attributeNames.hasMoreElements() ) {
+            println(attributeNames.nextElement())
+        }
+        val parametersNames = req.attributeNames
+        println("Имена параметров: ")
+        if(parametersNames.toList().isEmpty())
+            println("Не было найдено не одного параметра")
+        while(parametersNames.hasMoreElements() ) {
+            println(parametersNames.nextElement())
+        }
         resp.writer.print("Otvet dan! Your message: $requestText. Counter: ${counter++}")
     }
 
